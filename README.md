546dec674244fbdeb57014e50cbe1df50b2985b1
Rizky92 <myquinn489@gmail.com>
2023-03-07 08:20:37 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Helper file for `nesbot/carbon`
+Helper for `nesbot/carbon`
+
+## Installation
+`composer require rizky92/carbon-helper`
+
+## Usage
+there are 2 functions, `carbon` and `carbon_immutable`. The difference is that `carbon` mutates the date, while `carbon_immutable` does not. Please refer to [carbon documentation](https://carbon.nesbot.com/docs/) for more info.
+
+### Function `carbon`
+```php
+$christmas = carbon('December 25, 2022');
+$chrismas->addDays(6);
+
+echo $chrismas;  // 2022-12-31 00:00:00.0 Asia/Makassar (+08:00)
+```
+
+### Function `carbon_immutable`
+```php
+$christmas = carbon_immutable('December 25, 2022');
+$christmas->addDays(6);
+
+echo $chrismas;  // 2022-12-25 00:00:00.0 Asia/Makassar (+08:00)
+```
---
+++ src/helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+
+if (! function_exists('carbon')) {
+    /**
+     * @param string|DateTimeInterface|null $time
+     * @param DateTimeZone|string|null $tz
+     *
+     * @throws \Carbon\Exceptions\InvalidFormatException
+     *
+     * @return static
+     * 
+     * @see \Carbon\Carbon::parse()
+     */
+    function carbon($time = null, $tz = null)
+    {
+        return Carbon::parse($time, $tz);
+    }
+}
+
+if (! function_exists('carbon_immutable')) {
+    /**
+     * @param string|DateTimeInterface|null $time
+     * @param DateTimeZone|string|null $tz
+     *
+     * @throws \Carbon\Exceptions\InvalidFormatException
+     *
+     * @return static
+     * 
+     * @see \Carbon\CarbonImmutable::parse()
+     */
+    function carbon_immutable($time = null, $tz = null)
+    {
+        return CarbonImmutable::parse($time, $tz);
+    }
+}
